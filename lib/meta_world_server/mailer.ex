5c980f7a1826ea87d8d7b87f148ffa829bac7c51
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-07-25 10:22:20 +0900

    mix add credo and set up

--- lib/meta_world_server/mailer.ex
+++ lib/meta_world_server/mailer.ex
@@ -1,3 +1,4 @@
 defmodule MetaWorldServer.Mailer do
+  @moduledoc false
   use Swoosh.Mailer, otp_app: :meta_world_server
 end
--- lib/meta_world_server_web/telemetry.ex
+++ lib/meta_world_server_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule MetaWorldServerWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 

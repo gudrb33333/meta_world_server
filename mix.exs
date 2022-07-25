32b28a6d7cfa3908614bb411855e9c51bf24911b
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-07-25 10:32:09 +0900

    mix add coveralls and set up

--- mix.exs
+++ mix.exs
@@ -10,7 +10,15 @@ defmodule MetaWorldServer.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      # the two ex_coveralls items
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -49,7 +57,8 @@ defmodule MetaWorldServer.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14", only: [:dev, :test], runtime: false}
     ]
   end
 

4f1bbc07d8b67a5008bb1e897b4d3c1858a2e5b7
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-08-13 11:20:54 +0900

    add dev https setting

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,13 @@ config :meta_world_server, MetaWorldServer.Repo,
 config :meta_world_server, MetaWorldServerWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  https: [
+    ip: {127, 0, 0, 1},
+    port: 4000,
+    cipher_suite: :strong,
+    keyfile: "priv/cert/key.pem",
+    certfile: "priv/cert/cert.pem"
+  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
